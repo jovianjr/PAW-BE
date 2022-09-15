2b9eb6b1c6ed0867eb769c10b930de03d4dd8b1e
jovian <alphonsusjovian@gmail.com>
2022-09-15 19:31:12 +0700

    feat(setup): mongodb config

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 4000
+// add mod alias
+require('module-alias/register');
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+
+const connectDB = require('@src/config/database');
+
+// initialize config
+dotenv.config({ path: '.env' });
+connectDB();
+
+// initialize app
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Selamat Datang Di PAW!')
-})
+	res.send('Selamat Datang Di PAW!');
+});
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+// 404
+app.use((req, res, next) => {
+	res.status(404).send('NOT FOUND!');
+});
+
+// Global error handling
+app.use(function (err, _req, res) {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
+
+// server run
+app.listen(process.env.PORT, () => {
+	console.log(`Server running on port ${process.env.PORT}`);
+});
