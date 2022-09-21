a77759429062fd201c636df9142930914e23af19
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2022-09-22 06:12:20 +0700

    feat : menambah schema untuk artwork

--- src/models/Artwork.js
+++ src/models/Artwork.js
@@ -3,32 +3,19 @@ const mongoose = require('mongoose');
 // artwork schema
 const artworkSchema = new mongoose.Schema(
 	{
-		artwork_id: { type: String, unique: true },
-		title: {
-			type: String,
-			required: true,
-		},
+		title: { type: String,required: true,},
 		img: {
 			data: Buffer,
 			contentType: String,
 			required: true,
 		},
 		description: {
-			type: String,
-			required: true,
-		},
-		created_at: {
-			type: Date,
-			required: true,
+			type: String
 		},
 		artist: {
 			type: String,
 			required: true,
-		},
-		updated_at: {
-			type: Date.now,
-			required: true,
-		},
+		}
 	},
 	{
 		timestamps: true,
