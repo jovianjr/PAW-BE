ea070d830f96ec6d9c6b4dbb64242880c2151285
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2022-09-24 19:38:46 +0700

    feat : update filtering

--- src/controllers/Artwork.js
+++ src/controllers/Artwork.js
@@ -1,18 +1,15 @@
 const Artwork = require('@src/models/Artwork');
 
 const getArtworkController = async (req, res) => {
-    const displayArtwork = await Artwork.find(
-        
-    )
-    console.log(displayArtwork);
-    res.send("test test 123")
+    const findArtworkByArtist = await Artwork.find(
+        {artist: req.query.artist ?? null}
+    ).sort(req.query.sort).exec();
+
+    console.log(req.query.sort);
+    res.send(findArtworkByArtist);
 }
 
 const findArtworkController = async (req,res) => {
-    const findArtworkByArtist = await Artwork.find(
-        {artist : 'aulia'}
-    ).sort(req.params.artist).exec();
-    console.log(findArtworkByArtist);
-    res.send(findArtworkByArtist);
+    
 }
 module.exports = {getArtworkController, findArtworkController};
