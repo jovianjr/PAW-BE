4ee5c5c484e143b4d50c077b1c0992693e51232e
jovian <alphonsusjovian@gmail.com>
2022-09-25 13:40:21 +0700

    refactor(user): res tempalte && error handling

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,14 +1,22 @@
 const User = require('@src/models/User');
+const { sendError } = require('@src/helpers/response');
 
 const findUserController = async (req, res) => {
-	const findUserController = await User.find({
-		name: {
-			$regex: req.params.name,
-			$options: 'i',
-		},
-	});
+	try {
+		const data = await User.find({
+			name: {
+				$regex: req.params.name,
+				$options: 'i',
+			},
+		});
 
-	res.send(findUserController);
+		return res.json({
+			message: 'success',
+			data,
+		});
+	} catch (e) {
+		return sendError(res, 400, 'Something went wrong');
+	}
 };
 
 module.exports = { findUserController };
