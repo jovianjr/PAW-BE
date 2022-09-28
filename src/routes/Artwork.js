7382f5c548a30d109cd66c89fba2db40bb7da3e4
jovian <alphonsusjovian@gmail.com>
2022-09-28 23:29:09 +0700

    hotfix(artwork): add controller to routes

--- src/routes/Artwork.js
+++ src/routes/Artwork.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const {
 	findArtworkController,
 	findArtworkByIdController,
+	newArtworkController,
+	editArtworkController,
+	deleteArtworkController,
 } = require('@src/controllers/artwork');
 
 router.get('/', findArtworkController);
 router.get('/:id', findArtworkByIdController);
+router.post('/', newArtworkController);
+router.patch('/', editArtworkController);
+router.delete('/', deleteArtworkController);
 
 module.exports = router;
